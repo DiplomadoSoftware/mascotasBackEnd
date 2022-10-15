e6922d1582336c1c171d2eaaf2da39bd3a53930a
ADRIAN LOPEZ <adrianinsuasty@udenar.edu.co>
2022-10-14 22:55:33 -0500

    Add get,post,put,delete.(Request)

--- src/Models/petRequest.js
+++ src/Models/petRequest.js
@@ -1,31 +1,68 @@
-import { DataTypes } from "sequelize";
-import { sequelize } from "../Database/index.js";
+import { PetRequest } from '../Models/index.js';
 
-const petRequest = sequelize.define( 'solicitudes', {
-    id: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-    },
-    nombre_cliente: {
-        type: DataTypes.STRING
-    },
-    correo: {
-        type: DataTypes.STRING
-    },
-    telefono: {
-        type: DataTypes.STRING
-    },
-    fecha: {
-        type: DataTypes.DATE
-    },
-    estado: {
-        type: DataTypes.STRING
-    },
-    id_mascota: {
-        type: DataTypes.INTEGER,
-        references: 'mascotas',
-        referencesKey: 'id'
+const getPetRequests = async (req, res) => {
+    try {
+        const { id: petId } = req.params;
+        const petRequests = await PetRequest.findAndCountAll({
+            where: { mascota_id: petId }
+        });
+
+        res.status(200).json(petRequests);
+    } catch (error) {
+        res.status(400).json({ Error: `${error}` });
     }
-});
+}
 
-export { petRequest }
+const postPetRequest = async (req, res) => {
+    try {
+        const { id: petId } = req.params;
+        const { id, clientName, mail, phone, date, state } = req.body;
+        const newPetRequest = await PetRequest.create({
+            id,
+            nombre_cliente: clientName,
+            correo: mail,
+            telefono: phone,
+            fecha: date,
+            estado: state,
+            mascota_id: petId,
+        });
+
+        res.status(200).json(newPetRequest);
+    } catch (error) {
+        res.status(400).json({ Error: `${error}` });
+    }
+}
+
+const putPetRequest = async (req, res) => {
+    try {
+        const { idSol } = req.params;
+        const { clientName, mail, phone, date, state } = req.body;
+        const oldPetRequest = await PetRequest.findByPk(idSol);
+
+        // update incoming data
+        oldPetRequest.nombre_cliente = clientName || oldPetRequest.nombre_cliente;
+        oldPetRequest.correo = mail || oldPetRequest.correo;
+        oldPetRequest.telefono = phone || oldPetRequest.telefono;
+        oldPetRequest.fecha = date || oldPetRequest.fecha;
+        oldPetRequest.estado = state || oldPetRequest.estado;
+
+        const updatedPetRequest = await oldPetRequest.save();
+
+        res.status(200).json(updatedPetRequest);
+    } catch (error) {
+        res.status(400).json({ Error: `${error}` });
+    }
+}
+
+const deletePetRequest = async (req, res) => {
+    try {
+        const { idSol } = req.params;
+        await PetRequest.destroy({ where: { id: idSol } });
+
+        res.status(200).json({ body: { id: idSol } });
+    } catch (error) {
+        res.status(400).json({ Error: `${error}` });
+    }
+}
+
+export { getPetRequests, postPetRequest, putPetRequest, deletePetRequest }
