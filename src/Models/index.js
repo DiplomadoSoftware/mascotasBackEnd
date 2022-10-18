b519f7b45bb90a2b870629090cae99889a51ab39
ADRIAN LOPEZ <adrianinsuasty@udenar.edu.co>
2022-10-17 20:58:50 -0500

    Add sequelize define mascotas

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,2 +1,30 @@
-export pet from './pets';
-export petRequest from './petRequest';
+import { DataTypes } from "sequelize";
+import { sequelize } from "../Database/index.js";
+import { PetRequest } from "./petRequest.js";
+
+const Pet = sequelize.define( 'mascotas', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    nombre: {
+        type: DataTypes.STRING
+    },
+    tipo: {
+        type: DataTypes.STRING
+    },
+    estado: {
+        type: DataTypes.STRING
+    },
+    descripcion: {
+        type: DataTypes.STRING
+    },
+    raza: {
+        type: DataTypes.STRING
+    },
+
+},{sequelize, timestamps: false});
+
+Pet.hasMany(PetRequest,  { foreignKey: 'mascota_id' });
+
+export { Pet }
